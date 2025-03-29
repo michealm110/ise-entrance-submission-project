fe8d7abdca782c408e4d352e36138dd714991296
michealm110 <micheal.mcmagh@gmail.com>
2025-03-29 13:44:43 +0000

    iterative design part 2 database consists of eircode, longatitude and latitude

--- solarcalc/views.py
+++ solarcalc/views.py
@@ -1,6 +1,16 @@
 
 import flask
+import sqlite3
 from random import randrange
+import hashids
+import requests
+
+def db_get_connection():
+
+    con = sqlite3.connect('solarcalc.db')
+    con.execute("CREATE TABLE IF NOT EXISTS simulation (id INTEGER PRIMARY KEY AUTOINCREMENT, eircode TEXT NOT NULL , latitude_longitude TEXT);")
+    con.commit()
+    return con
 
 app = flask.Flask(__name__)
 
@@ -8,19 +18,33 @@ app = flask.Flask(__name__)
 def index():
     if flask.request.method == "POST":
         eircode = flask.request.form.get('eircode', '')   
-        return flask.render_template('index.html', eircode=eircode) #this isnt actually want   
+        lat_lon = get_lat_lon_from_eircode(eircode)
+        lat_lon_string = ",".join(map(str, lat_lon))
+        con = db_get_connection()
+        cur = con.cursor()
+        cur.execute("INSERT INTO simulation (eircode , latitude_longitude) VALUES (?,?)", (eircode, lat_lon_string))
+        last_row_id = cur.lastrowid
+        con.commit()
+        con.close()
+        hash_id = make_random_name(last_row_id)
+        #return flask.render_template('index.html', eircode=hash_id)
+        return flask.redirect(flask.url_for("show_data", hash_id=hash_id))
+
     elif flask.request.method == "GET":
         return flask.render_template('index.html')
     else:
         #boom!
         raise
+    
+@app.route("/<hash_id>")
+def show_data(hash_id):
+    return flask.render_template("displayhashid.html", hash_id=hash_id)
 
-
-def make_random_name() -> str:
-    # 8 charachters [a-z0-9]
-
-    #using ascii and randrange
-    #Three sections; 0-9 (10/62), A-Z (26/62), a-z(26/62)
+def make_random_name(n: int) -> str:
+    '''
+        # 8 charachters [a-z0-9]
+    # using ascii and randrange
+    # two sections; 0-9 (10/36),a-z(26/36)
     x = ""
     for i in range(8):
         num = randrange(0,36)
@@ -30,4 +54,25 @@ def make_random_name() -> str:
         elif 10 <= num <= 35:
             char = 97 + (num - 10)
         x += chr(char)
-    return x
+    #salt, hash it, base36 it
+    '''
+    alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+    hash_encoder = hashids.Hashids(salt='this is my salt', min_length=8, alphabet=alphabet)
+    hashid = hash_encoder.encode(n)
+
+    return hashid
+
+
+
+def get_lat_lon_from_eircode(eircode):
+    base_url = "https://nominatim.openstreetmap.org/search"
+    headers = {"User-Agent": "MickSolarCalc/1.0"} 
+    params = {"q": eircode, "format": "json"}
+    response = requests.get(base_url, headers=headers, params=params)
+    data = response.json()
+
+    if data:
+        return float(data[0]["lat"]), float(data[0]["lon"])
+    else:
+        return None
+
