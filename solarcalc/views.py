7f45d996483f937b1fdff9c2eb1d48da6dee4050
michealm110 <micheal.mcmagh@gmail.com>
2025-03-31 11:02:06 +0100

    adds first attempt at calculating solar panel output
    adds basic plotting with matplotlib
    adds a jupyter notebook for exploration
    adds solar panel parameter to form on index page
    adds a basic calculation of output to our simulation view
    adds a basic layout in a jinja base template
    exploration of chartjs

--- solarcalc/views.py
+++ solarcalc/views.py
@@ -1,14 +1,18 @@
 
 import flask
 import sqlite3
-from random import randrange
 import hashids
 import requests
+import pvlib
+import pandas
+from datetime import datetime, timedelta, timezone
+import pytz
+import os
 
 def db_get_connection():
 
     con = sqlite3.connect('solarcalc.db')
-    con.execute("CREATE TABLE IF NOT EXISTS simulation (id INTEGER PRIMARY KEY AUTOINCREMENT, eircode TEXT NOT NULL , latitude_longitude TEXT);")
+    con.execute("CREATE TABLE IF NOT EXISTS simulation (id INTEGER PRIMARY KEY AUTOINCREMENT, eircode TEXT NOT NULL , latitude_longitude TEXT, panel_area REAL, panel_efficiency REAL, panel_tilt REAL, panel_azimuth REAL);")
     con.commit()
     return con
 
@@ -17,17 +21,25 @@ app = flask.Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if flask.request.method == "POST":
-        eircode = flask.request.form.get('eircode', '')   
+
+        # Getting and storing all the inputs inputted on index.html
+        eircode = flask.request.form.get('eircode')   
         lat_lon = get_lat_lon_from_eircode(eircode)
         lat_lon_string = ",".join(map(str, lat_lon))
+        panel_area = flask.request.form.get('panel_area')
+        panel_efficiency = int(flask.request.form.get('panel_efficiency')) / 100
+        panel_tilt = flask.request.form.get('panel_tilt')
+        panel_azimuth = flask.request.form.get('panel_azimuth')
+
+        # Save all these inputs to the solcalc.db database
         con = db_get_connection()
         cur = con.cursor()
-        cur.execute("INSERT INTO simulation (eircode , latitude_longitude) VALUES (?,?)", (eircode, lat_lon_string))
+        cur.execute("INSERT INTO simulation (eircode , latitude_longitude, panel_area, panel_efficiency, panel_tilt, panel_azimuth) VALUES (?,?,?,?,?,?)", (eircode, lat_lon_string, panel_area, panel_efficiency, panel_tilt, panel_azimuth))
         last_row_id = cur.lastrowid
         con.commit()
         con.close()
+
         hash_id = make_random_name(last_row_id)
-        #return flask.render_template('index.html', eircode=hash_id)
         return flask.redirect(flask.url_for("show_data", hash_id=hash_id))
 
     elif flask.request.method == "GET":
@@ -38,30 +50,37 @@ def index():
     
 @app.route("/<hash_id>")
 def show_data(hash_id):
-    return flask.render_template("displayhashid.html", hash_id=hash_id)
+    # Decode users hash_id
+    # Should do this in a function ideally
+    alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+    hash_decoder = hashids.Hashids(salt='this is my salt', min_length=8, alphabet=alphabet)
+    decoded_id = hash_decoder.decode(hash_id)[0]
+    # .decoder() returns a tuple
+
+
+    times_from_now = rounds_and_calculates_a_year_of_dates()
+    # Get data from solarcalc.db database and plug the data into calc_power_output() 
+    con = db_get_connection()
+    cur = con.cursor()
+    # Not worth individually assiging each parameter grabbing them all as a tuple
+    cur.execute("SELECT * FROM simulation WHERE id = ?", (decoded_id,))
+    decoded_id_row_from_db = cur.fetchone()
+    con.close()
+
+    # Converting the latitude_longitude to a tuple
+    lat_lon = tuple(map(float, decoded_id_row_from_db[2].split(",")))
+
+    dc_power_output = calc_power_output(lat_lon[0], lat_lon[1], decoded_id_row_from_db[3], decoded_id_row_from_db[4], decoded_id_row_from_db[5], decoded_id_row_from_db[6], times_from_now)
+    os.makedirs('solarcalc/static', exist_ok=True)
+    dc_power_output.to_json("solarcalc/static/data.json", orient="records")
+    return flask.render_template("simulator.html", power_output=dc_power_output)
 
 def make_random_name(n: int) -> str:
-    '''
-        # 8 charachters [a-z0-9]
-    # using ascii and randrange
-    # two sections; 0-9 (10/36),a-z(26/36)
-    x = ""
-    for i in range(8):
-        num = randrange(0,36)
-        char = 0
-        if 0 <= num <= 9:
-            char = 48 + num
-        elif 10 <= num <= 35:
-            char = 97 + (num - 10)
-        x += chr(char)
     #salt, hash it, base36 it
-    '''
     alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
     hash_encoder = hashids.Hashids(salt='this is my salt', min_length=8, alphabet=alphabet)
     hashid = hash_encoder.encode(n)
-
     return hashid
-
 
 
 def get_lat_lon_from_eircode(eircode):
@@ -75,4 +94,57 @@ def get_lat_lon_from_eircode(eircode):
         return float(data[0]["lat"]), float(data[0]["lon"])
     else:
         return None
+    
+# Function that calculates power output of a solar panel 
+# Makes assumptions about weather
+# date and time not constant for this project
+def calc_power_output(latitude, longitude, panel_area, panel_efficiency, panel_tilt, panel_azimuth, times):
+    tz = 'Europe/Dublin'
+    location = pvlib.location.Location(latitude, longitude)
+    #clearsky_irradiance =  location.get_clearsky(times)
+
+    data, _, _ , _ = pvlib.iotools.get_pvgis_tmy(latitude=latitude, longitude=longitude)
+
+    solar_position = location.get_solarposition(data.index)
+
+
+    # solar_position = location.get_solarposition(date_time)
+    # Optimisic estimate of solar irradiation because it assumes clear skies
+    # clear_sky = location.get_clearsky(date_time, model="ineichen")
+    irradiance = pvlib.irradiance.get_total_irradiance(
+        surface_tilt=panel_tilt,
+        surface_azimuth=panel_azimuth,
+        solar_zenith=solar_position['zenith'],
+        solar_azimuth=solar_position['azimuth'],
+        ghi=data['ghi'],
+        dni=data['dni'],
+        dhi=data['dhi'])
+    
+    # default values entered for wind_speed, u0, and u1
+    module_temperature = pvlib.temperature.faiman(poa_global=irradiance["poa_global"], temp_air=data["temp_air"], wind_speed=1, u0=25.0, u1=6.84)
+
+    dc_power = pvlib.pvsystem.pvwatts_dc(
+    g_poa_effective=irradiance['poa_global'],
+    # simplified assumptions that module and cell temperature are the same
+    temp_cell=module_temperature,
+    pdc0=300,
+    gamma_pdc=-0.004,
+    temp_ref=25.0,
+    )
+
+    dc_power.index = dc_power.index.map(lambda x: x.replace(year=2024))
+
+
+
+    # Returns power output of your solar panel in Watts at a specific location
+    return dc_power
+
+def rounds_and_calculates_a_year_of_dates():
+    tz = "Europe/Dublin"
+    #times = pd.date_range(start='2025-03-29 00:00:00', end='2025-03-30 00:00:00', freq='30min', tz='Europe/Dublin')
+    tz_formatted = pytz.timezone(tz)
+    time_now_aware = datetime.now(tz_formatted)
+    delta = timedelta(minutes=30)
+    time_rounded_down = time_now_aware - (time_now_aware - datetime.min.replace(tzinfo=timezone.utc)) % delta
+    return pandas.date_range(start=time_rounded_down.replace(year=(int(time_rounded_down.year) - 1)), end=time_rounded_down, freq="30min", tz=tz)
 
