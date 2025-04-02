2d29d91ac5ce9146623dcf0f0b2f4ad5d00fed2b
michealm110 <micheal.mcmagh@gmail.com>
2025-04-02 12:18:39 +0100

    redid the calculate solar panel output function

--- solarcalc/views.py
+++ solarcalc/views.py
@@ -14,7 +14,7 @@ ALLOWED_EXTENSIONS = {"csv"}
 
 def db_get_connection():
     con = sqlite3.connect("solarcalc.db")
-    con.execute("CREATE TABLE IF NOT EXISTS simulation (id INTEGER PRIMARY KEY AUTOINCREMENT, eircode TEXT NOT NULL , latitude_longitude TEXT, panel_area REAL, panel_efficiency REAL, panel_tilt REAL, panel_azimuth REAL);")
+    con.execute("CREATE TABLE IF NOT EXISTS simulation (id INTEGER PRIMARY KEY AUTOINCREMENT, eircode TEXT NOT NULL , latitude_longitude TEXT, rated_power_per_panel REAL, number_of_panels INTEGER, panel_tilt REAL, panel_azimuth REAL);")
     con.commit()
     return con
 
@@ -56,21 +56,20 @@ def index():
 def get_detailed_user_data(hash_id):
     if flask.request.method == "POST":
         decoded_id = decode_id(hash_id)
-        panel_area = flask.request.form.get("panel_area")
-        panel_efficiency = int(flask.request.form.get("panel_efficiency")) / 100
+        rated_power_per_panel = flask.request.form.get("rated_power_per_panel")
+        number_of_panels = flask.request.form.get("number_of_panels")
         panel_tilt = flask.request.form.get("panel_tilt")
         panel_azimuth = flask.request.form.get("panel_azimuth")
-        #esb_file = flask.request.form.get("esb_file")
 
 
         # Update data base with new inputs
         con = db_get_connection()
         cur = con.cursor()
-        cur.execute("UPDATE simulation SET panel_area = ?, panel_efficiency = ?, panel_tilt = ?, panel_azimuth = ? WHERE id = ?", (panel_area, panel_efficiency, panel_tilt, panel_azimuth, decoded_id))   
+        cur.execute("UPDATE simulation SET rated_power_per_panel = ?, number_of_panels = ?, panel_tilt = ?, panel_azimuth = ? WHERE id = ?", (rated_power_per_panel, number_of_panels, panel_tilt, panel_azimuth, decoded_id))   
         con.commit()
         con.close()
 
-        return flask.redirect(flask.url_for("simulate", hash_id=hash_id))
+        return flask.render_template("furtherdetails.html", hash_id=hash_id)
     elif flask.request.method == "GET":
         con = db_get_connection()
         cur = con.cursor()
@@ -80,9 +79,9 @@ def get_detailed_user_data(hash_id):
         con.close()
 
         if row is not None:
-            return flask.render_template("furtherdetails.html", hash_id=hash_id, panel_area=row[3], panel_efficiency=row[4], panel_tilt=row[5], panel_azimuth=row[6])
+            return flask.render_template("furtherdetails.html", hash_id=hash_id, rated_power_per_panela=row[3], number_of_panelsy=row[4], panel_tilt=row[5], panel_azimuth=row[6])
         
-        return flask.render_template("furtherdetails.html", hash_id=hash_id, panel_area=15, panel_efficiency=18, panel_azimuth=180, panel_tilt=18)
+        return flask.render_template("furtherdetails.html", hash_id=hash_id, rated_power_per_panel=445, number_of_panels=1, panel_azimuth=180, panel_tilt=18)
     else:
         raise
 
@@ -90,7 +89,7 @@ def get_detailed_user_data(hash_id):
 @app.route("/<hash_id>/solardata")
 def get_json_data(hash_id):
     decoded_id = decode_id(hash_id)
-    times_from_now = rounds_and_calculates_a_year_of_dates()
+    # times_from_now = rounds_and_calculates_a_year_of_dates()
     # Get data from solarcalc.db database and plug the data into calc_power_output() 
     con = db_get_connection()
     cur = con.cursor()
@@ -103,7 +102,7 @@ def get_json_data(hash_id):
     lat_lon = tuple(map(float, decoded_id_row_from_db[2].split(",")))\
     
  
-    dc_power_output = calc_power_output(lat_lon[0], lat_lon[1], decoded_id_row_from_db[3], decoded_id_row_from_db[4], decoded_id_row_from_db[5], decoded_id_row_from_db[6], times_from_now)
+    dc_power_output = calc_power_output(lat_lon[0], lat_lon[1], decoded_id_row_from_db[3], decoded_id_row_from_db[4], decoded_id_row_from_db[5], decoded_id_row_from_db[6])
     dc_power_output = dc_power_output.reset_index().rename(columns={"time(UTC)": "x", 0: "y"})
 
     dc_power_output = dc_power_output[(dc_power_output["x"] >= "2024-07-23T00:00:00.000") & (dc_power_output["x"] < "2024-07-24T00:00:00.000")]
@@ -117,6 +116,8 @@ def get_esb_json_data(hash_id):
     esb_intake = pandas.read_csv(f"/Users/michealmcmagh/Desktop/ise-entrance-submission-project/solarcalc/uploads/{hash_id}.csv")
     esb_intake = pandas.concat([esb_intake["Read Date and End Time"], esb_intake["Read Value"]], join="inner", axis=1)
     esb_intake = esb_intake.rename(columns={"Read Date and End Time": "x", "Read Value": "y"})
+    # Converts killowatts to watts
+    esb_intake["y"] = esb_intake["y"] * 1000
     esb_intake["x"] = pandas.to_datetime(esb_intake["x"], dayfirst=True).dt.tz_localize('UTC')
     esb_intake = esb_intake.sort_values(by=['x'])
     esb_intake = esb_intake[(esb_intake["x"] >= "2024-07-23T00:00:00.000") & (esb_intake["x"] < "2024-07-24T00:00:00.000")]
@@ -183,47 +184,51 @@ def get_lat_lon_from_eircode(eircode):
 # Function that calculates power output of a solar panel 
 # Makes assumptions about weather
 # date and time not constant for this project
-def calc_power_output(latitude, longitude, panel_area, panel_efficiency, panel_tilt, panel_azimuth, times):
-    tz = "Europe/Dublin"
+def calc_power_output(latitude, longitude, rated_power_per_panel, number_of_panels, panel_tilt, panel_azimuth):    
     location = pvlib.location.Location(latitude, longitude)
     #clearsky_irradiance =  location.get_clearsky(times)
 
-    data, _, _ , _ = pvlib.iotools.get_pvgis_tmy(latitude=latitude, longitude=longitude)
+    # Typical meteorogical year using the pvgis api
+    data, _, _, _ = pvlib.iotools.get_pvgis_tmy(latitude=latitude, longitude=longitude)
 
     solar_position = location.get_solarposition(data.index)
 
-
-    # solar_position = location.get_solarposition(date_time)
+    #solar_position = location.get_solarposition(date_time)
     # Optimisic estimate of solar irradiation because it assumes clear skies
     # clear_sky = location.get_clearsky(date_time, model="ineichen")
+
+    # Calculates the irradiance watts per m squared of the panel surface
+    # Pvlib assumes a gorund albedo of around .2 which is correspondant with grass and soil, may differ for highly reflective surfaces like snow
     irradiance = pvlib.irradiance.get_total_irradiance(
         surface_tilt=panel_tilt,
         surface_azimuth=panel_azimuth,
-        solar_zenith=solar_position["zenith"],
-        solar_azimuth=solar_position["azimuth"],
-        ghi=data["ghi"],
-        dni=data["dni"],
-        dhi=data["dhi"])
+        solar_zenith=solar_position['zenith'],
+        solar_azimuth=solar_position['azimuth'],
+        ghi=data['ghi'],
+        dni=data['dni'],
+        dhi=data['dhi'])
     
-    # default values entered for wind_speed, u0, and u1
+    #default values entered for wind_speed, u0, and u1
     module_temperature = pvlib.temperature.faiman(poa_global=irradiance["poa_global"], temp_air=data["temp_air"], wind_speed=1, u0=25.0, u1=6.84)
 
     dc_power = pvlib.pvsystem.pvwatts_dc(
-    g_poa_effective=irradiance["poa_global"],
+    g_poa_effective=irradiance['poa_global'],
     # simplified assumptions that module and cell temperature are the same
     temp_cell=module_temperature,
-    pdc0=300,
+    pdc0=rated_power_per_panel,
     gamma_pdc=-0.004,
     temp_ref=25.0,
     )
-
+    #dc_power.index = dc_power.index.replace(year=2024)
     dc_power.index = dc_power.index.map(lambda x: x.replace(year=2024))
 
+    # Plot x-axis: time, y-axis: dc_power on a line chart thingy
+    #matplotlib.pyplot.plot
+    
 
+    return dc_power * number_of_panels
 
-    # Returns power output of your solar panel in Watts at a specific location
-    return dc_power
-
+'''
 # This function was misinformed in its creation going to keep it in until i figure out if its completely misguided or not
 def rounds_and_calculates_a_year_of_dates():
     tz = "Europe/Dublin"
@@ -232,4 +237,5 @@ def rounds_and_calculates_a_year_of_dates():
     time_now_aware = datetime.now(tz_formatted)
     delta = timedelta(minutes=30)
     time_rounded_down = time_now_aware - (time_now_aware - datetime.min.replace(tzinfo=timezone.utc)) % delta
-    return pandas.date_range(start=time_rounded_down.replace(year=(int(time_rounded_down.year) - 1)), end=time_rounded_down, freq="30min", tz=tz)
+    return pandas.date_range(start=time_rounded_down.replace(year=(int(time_rounded_down.year) - 1)), end=time_rounded_down, freq="30min", tz=tz)'
+'''
